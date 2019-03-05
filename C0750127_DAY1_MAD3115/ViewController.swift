382be165cd688d13f9c8b76894e26bd443ccafba
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-03-04 21:17:52 -0500

    calculator first ios app.

--- C0750127_DAY1_MAD3115/ViewController.swift
+++ C0750127_DAY1_MAD3115/ViewController.swift
@@ -12,7 +12,42 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var IblMessage: UILabel!
     @IBOutlet weak var IblMessageTwo: UILabel!
+    @IBOutlet weak var txtNumber1: UITextField!
+    @IBOutlet weak var txtNumber2: UITextField!
     
+    @IBAction func BtnSubmitClick(_ sender: UIButton) {
+        IblMessageTwo.text = "Button Clicked!"
+    }
+    
+    @IBAction func btnPlus(_ sender: UIButton) {
+        let textfieldInt: Int? = Int(txtNumber1.text!)
+        let textfield2Int: Int? = Int(txtNumber2.text!)
+        let convert = textfieldInt! + textfield2Int!
+        let convertText = String(convert)
+        IblMessageTwo.text = convertText
+    }
+    
+    @IBAction func btnMinus(_ sender: UIButton) {
+        let textfieldInt: Int? = Int(txtNumber1.text!)
+        let textfield2Int: Int? = Int(txtNumber2.text!)
+        let convert = textfieldInt! - textfield2Int!
+        let convertText = String(convert)
+        IblMessageTwo.text = convertText
+    }
+    @IBAction func btnMult(_ sender: UIButton) {
+        let textfieldInt: Int? = Int(txtNumber1.text!)
+        let textfield2Int: Int? = Int(txtNumber2.text!)
+        let convert = textfieldInt! * textfield2Int!
+        let convertText = String(convert)
+        IblMessageTwo.text = convertText
+    }
+    @IBAction func btnDivide(_ sender: UIButton) {
+        let textfieldInt: Int? = Int(txtNumber1.text!)
+        let textfield2Int: Int? = Int(txtNumber2.text!)
+        let convert = textfieldInt! / textfield2Int!
+        let convertText = String(convert)
+        IblMessageTwo.text = convertText
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         IblMessage.text = "Hello World !"
